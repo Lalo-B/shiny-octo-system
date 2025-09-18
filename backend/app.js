bb6ce1b528d9b064de1f02186fde840aaede4aaf
Gerardo_Bonilla_Jr <g830bo@gmail.com>
2025-09-17 21:34:56 -0700

    finished for today, will work on user auth next

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-require('express-async-errors');
 const morgan = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
@@ -8,12 +7,13 @@ const cookieParser = require('cookie-parser');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 const routes = require('./routes');
+const {ValidationError} = require('sequelize');
 
 const app = express();
 app.use(morgan('dev')); //connects morgan to log info on reqs and res'
 app.use(cookieParser());
 app.use(express.json());
-app.use(routes);
+
 
 if (!isProduction) {   // enable cors only in development
   app.use(cors());
@@ -36,6 +36,40 @@ app.use(
     }
   })
 );
+
+app.use(routes);
+
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 
 module.exports = app;
