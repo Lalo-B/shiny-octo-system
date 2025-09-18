6a4eddf312fb0152ec98808d5f58889a10d76412
Gerardo_Bonilla_Jr <g830bo@gmail.com>
2025-09-18 14:30:54 -0700

    this should fix it

--- backend/db/migrations/20250918043705-create-user.js
+++ backend/db/migrations/20250918043705-create-user.js
@@ -42,6 +42,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Users"
-    await queryInterface.dropTable('Users', options);
+    return await queryInterface.dropTable(options);
   }
 };
