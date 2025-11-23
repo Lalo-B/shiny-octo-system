ec4f8fe46c9b57628b39035593c8ca04a4e1dc94
Gerardo_Bonilla_Jr <g830bo@gmail.com>
2025-11-23 16:33:41 -0700

    fixed modal

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,10 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
+import { Modal,ModalProvider } from './context/Modal';
 
 
-if(import.meta.env.MODE !== 'production') {
+if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
@@ -18,8 +19,11 @@ if(import.meta.env.MODE !== 'production') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
